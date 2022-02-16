d2301da5739724591b761f8fceb2bc42fc24628c
Matheus Crema <matheus.l.crema@gmail.com>
2022-02-16 19:54:06 -0300

    Added first Test class.

---
+++ API.Tests/CategoriesTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+using API.Controllers;
+
+namespace API.Tests
+{
+    public class CategoriesTests
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+        //    CategoriesController categoriesController = new CategoriesController();
+        //}
+    }
+}
