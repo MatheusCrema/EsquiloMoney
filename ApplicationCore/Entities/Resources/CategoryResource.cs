4270e6abb3019eee55e7b2725beaad5676339597
Matheus Crema <matheus.l.crema@gmail.com>
2022-02-26 20:46:32 -0300

    Included CategoryParentID in CategoriesController return.

--- ApplicationCore/Entities/Resources/CategoryResource.cs
+++ ApplicationCore/Entities/Resources/CategoryResource.cs
@@ -8,13 +8,10 @@ namespace ApplicationCore.Entities.Resources
         public int CategoryID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
         public int Hierarchy { get; set; }
-
         public string IconUI { get; set; }
-
         public DateTime CreatedDT { get; set; }
-
+        public int? CategoryParentID { get; set; }
         public List<CategoryBalance> CategoryBalances { get; set; } = new List<CategoryBalance>();
     }
 }
